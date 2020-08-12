3bc1e9e8388414549ad5055b4d4f1fb3d438a2c9
janvandenpoel <jan.vandenpoel@vdpc.be>
2020-08-12 14:33:59 +0200

    Chapter 01 - todo - Google Map

--- TripLog/TripLog/App.xaml.cs
+++ TripLog/TripLog/App.xaml.cs
@@ -2,6 +2,9 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using TripLog.Views;
+
+
 namespace TripLog
 {
     public partial class App : Application
@@ -10,7 +13,7 @@ namespace TripLog
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
