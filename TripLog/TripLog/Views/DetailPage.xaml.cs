53e62505567a98e31245d886c0fdb635da4fb7ed
janvandenpoel <jan.vandenpoel@vdpc.be>
2020-08-12 17:07:11 +0200

    Chapter 02

--- TripLog/TripLog/Views/DetailPage.xaml.cs
+++ TripLog/TripLog/Views/DetailPage.xaml.cs
@@ -9,29 +9,28 @@ using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 using TripLog.Models;
+using TripLog.ViewModels;
 
 namespace TripLog.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailPage : ContentPage
     {
+        DetailViewModel ViewModel => BindingContext as DetailViewModel;
         public DetailPage(TripLogEntry entry)
         {
-InitializeComponent();
+            InitializeComponent();
+            BindingContext = new DetailViewModel(entry);
             map.MoveToRegion(MapSpan.FromCenterAndRadius(
-                new Position(entry.Latitude,
-                    entry.Longitude),
+                new Position(ViewModel.Entry.Latitude,
+                     ViewModel.Entry.Longitude),
                     Distance.FromMiles(.5)));
             map.Pins.Add(new Pin
             {
                 Type = PinType.Place,
-                Label = entry.Title,
-                Position = new Position(entry.Latitude, entry.Longitude)
+                Label = ViewModel.Entry.Title,
+                Position = new Position(ViewModel.Entry.Latitude, ViewModel.Entry.Longitude)
             });
-            title.Text = entry.Title;
-            date.Text = entry.Date.ToString("M");
-            rating.Text = $"{entry.Rating} star rating";
-            notes.Text = entry.Notes;
         }
     }
 }
